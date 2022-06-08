edbf3c07a7172f1d5dbcbf30ca59435d94b692b3
Vivienne Ansah <voa2000@hotmail.com>
2022-06-08 22:03:39 +0100

    Add project files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './images/cbf_logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p>
+          DevOps Cloud Course
+      </p>
+      </header>
+      <body className="App-body">
+      <img src={logo} className="App-logo" alt="logo" />
+      </body>
+    </div>
+  );
+}
+
+export default App;
